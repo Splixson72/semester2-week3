188620a8966ea4f4e714e89d37e3b2465b6a7dd9
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2026-03-13 11:42:03 +0000

    worksheet done

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,34 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name:Zaryaab Ullah
+ * ID:201992194
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
+   float salary = 30000;
+   float original_salary = salary;
+   float national_insurance = 0.09;
+   float tax_rate = 0.25;
 
     // calculate the deductions and final take-home salary
-
-    // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+   float nat_deduction = salary * national_insurance;
+   salary -= nat_deduction;
+   float tax_deduction = 0;
+   if(salary>=12500)
+   {
+      salary -= 12500;
+      tax_deduction = salary * tax_rate;
+      salary -= tax_deduction;
+      salary += 12500;
+   }
+   
+    printf("Salary £%.2f\n",original_salary);
+    printf("NI contribution £%.2f\n",nat_deduction);
+    printf("Tax contribution £%.2f\n",tax_deduction);
+    printf("Take home salary £%.2f\n",salary);
 
     return 0;
  }
